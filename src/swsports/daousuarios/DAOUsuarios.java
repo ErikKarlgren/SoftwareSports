96375eae089ec20de42afee2468b82f1e7c62fc6
ErikKarlgren <erik.rivas8@gmail.com>
2020-05-06 11:37:40 +0200

    Añadido métodos de interfaces en módulo Usuarios y DAOUsuarios.
    También he añadido dichos métodos a las implementaciones pero están aún
    por implementar.

--- src/swsports/daousuarios/DAOUsuarios.java
+++ src/swsports/daousuarios/DAOUsuarios.java
@@ -1,5 +1,63 @@
 package swsports.daousuarios;
 
+import java.util.List;
+
+import swsports.modelo.Usuario;
+
 class DAOUsuarios implements IDAOUsuarios {
+
+	@Override
+	public boolean registroUsuario(Usuario usu) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean login(String id, String contrasenya) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Usuario> busquedaUsuarios(String nombre, String mail, String id, int telefono, String direccion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Usuario consultaUsuario(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean darDeBajaUsuario(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean editarUsuario(Usuario usu) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean hacerAdmin(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean quitarAdmin(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean cerrarSesion() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
--- src/swsports/daousuarios/IDAOUsuarios.java
+++ src/swsports/daousuarios/IDAOUsuarios.java
@@ -1,5 +1,25 @@
 package swsports.daousuarios;
 
-interface IDAOUsuarios {
+import java.util.List;
 
+import swsports.modelo.Usuario;
+
+interface IDAOUsuarios {
+	public boolean registroUsuario(Usuario usu);
+
+	public boolean login(String id, String contrasenya);
+
+	public List<Usuario> busquedaUsuarios(String nombre, String mail, String id, int telefono, String direccion);
+
+	public Usuario consultaUsuario(String id);
+
+	public boolean darDeBajaUsuario(String id);
+
+	public boolean editarUsuario(Usuario usu);
+
+	public boolean hacerAdmin(String id);
+
+	public boolean quitarAdmin(String id);
+
+	public boolean cerrarSesion();
 }
