20d274402d5c709a5f5d1ce8262159a44c35ca8e
javigom <64856260+javigom@users.noreply.github.com>
2020-05-20 20:02:02 +0200

    Update Carrito.java

--- src/swsports/modelo/Carrito.java
+++ src/swsports/modelo/Carrito.java
@@ -11,6 +11,9 @@ public class Carrito {
     this.listaProductos = new HashMap<Producto, Integer>();
   }
   
+  /*
+  * Añade un producto p al carrito. Si el producto ya está, simplemente se actualiza el número de unidades
+  */
   public anyadirProducto(Producto p){
     if(this.listaProductos.containsValue(p)){
       this.listaProductos.put(p, this.listaProductos.get(p) + 1);
