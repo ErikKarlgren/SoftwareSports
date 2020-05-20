3ebcbd91b299be3d77c0db46ef769c848ec81813
javigom <64856260+javigom@users.noreply.github.com>
2020-05-20 19:26:13 +0200

    Update FachadaDAOProductos.java

--- src/swsports/daoproductos/FachadaDAOProductos.java
+++ src/swsports/daoproductos/FachadaDAOProductos.java
@@ -10,47 +10,47 @@ public class FachadaDAOProductos implements IFachadaDAOProductos  {
   private IDAOProductos dao;
   
   public FachadaDAOProductos(){
-    dao= new DAOProductos();
+  	dao= new DAOProductos();
   }
   
   @Override
   public boolean altaProducto(Producto prod){
-       return dao.altaProducto(prod);
+  	return dao.altaProducto(prod);
   }
   
   @Override
   public boolean bajaProducto(Producto prod){
-		return dao.bajaProducto(prod);
+  	return dao.bajaProducto(prod);
   }
   
   @Override 
   public boolean editarProducto(Transfer prod){
-		return dao.editarProducto(prod);
+  	return dao.editarProducto(prod);
   }
   
   @Override 
   public Producto consultaProducto(String id){
-    return dao.consulta(id);
+  	return dao.consulta(id);
   }
   
   @Override
   public List<Producto> busquedaProducto(TransferProducto tProd){
-      return dao.busquedaProducto(null);
-	}
+  	return dao.busquedaProducto(null);
+  }
   
   @Override
   public boolean anyadirProducto(Producto prod){
-	  return dao.anyadirProducto(prod);
+  	return dao.anyadirProducto(prod);
   }
   
   @Override 
   public boolean quitarProducto(Producto prod){
-	  return dao.quitarProducto(prod);
+	return dao.quitarProducto(prod);
   }
   
   @Override 
   public boolean comprar(Tarjeta t, List<Producto> lp){
-	  return dao.comprar(t, lp);
+	return dao.comprar(t, lp);
   } 
 
 }
