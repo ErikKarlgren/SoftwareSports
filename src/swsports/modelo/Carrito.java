30c732794dabab2850e325d679e94811b193e779
javigom <64856260+javigom@users.noreply.github.com>
2020-05-20 20:00:58 +0200

    Update Carrito.java

--- src/swsports/modelo/Carrito.java
+++ src/swsports/modelo/Carrito.java
@@ -1,5 +1,24 @@
 package swsports.modelo;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class Carrito {
 
+  private Map<Producto, Integer> listaProductos;
+  
+  public Carrito(){
+    this.listaProductos = new HashMap<Producto, Integer>();
+  }
+  
+  public anyadirProducto(Producto p){
+    if(this.listaProductos.containsValue(p)){
+      this.listaProductos.put(p, this.listaProductos.get(p) + 1);
+    }
+    
+    else{
+      this.listaProductos.put(p, 1);
+    }
+  }
+  
 }
