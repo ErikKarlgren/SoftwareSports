177da31924a1865020c2b548057446594a4eaee6
javigom <64856260+javigom@users.noreply.github.com>
2020-05-26 19:25:26 +0200

    Add files via upload

--- src/swsports/gui/productos/PanelMainProductos.java
+++ src/swsports/gui/productos/PanelMainProductos.java
@@ -1,7 +1,93 @@
 package swsports.gui.productos;
 
-import javax.swing.JPanel;
+import java.awt.Color;
+import java.util.LinkedHashMap;
 
-public class PanelMainProductos extends JPanel {
+import javax.swing.BoxLayout;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+
+import swsports.gui.AbstractPanelMain;
+import swsports.modelo.TransferProducto;
+import swsports.modelo.Producto;
+import swsports.productos.ControladorProductos;
+
+public class PanelMainProductos extends AbstractPanelMain<Producto> {
+	
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		frame.add(new PanelMainProductos(new ControladorProductos()));
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	private JTextField idTextField;
+	private JTextField nombreTextField;
+	private JTextField descTextField;
+	private JTextField stockTextField;
+	private JTextField precioTextField;
+	private ControladorProductos controlador;
+
+	private class BuscarUsuarioWorker extends BuscarSwingWorker {
+
+		@Override
+		protected Void doInBackground() throws Exception {
+			setSearchButtonEnabled(false);
+
+			String id = idTextField.getText().equals("") ? null : idTextField.getText();
+			String nombre = nombreTextField.getText().equals("") ? null : nombreTextField.getText();
+			String desc =  descTextField.getText().equals("") ? null : descTextField.getText();
+			Integer stock = stockTextField.getText().equals("") ? null : Integer.valueOf(stockTextField.getText());
+			Double precio = precioTextField.getText().equals("") ? null : Double.valueOf(precioTextField.getText());
+
+			TransferProducto tProd = new TransferProducto(id, nombre, desc, stock, precio);
+			objetos = controlador.busquedaProducto(tProd);
+
+			removeReportablePanels();
+			for (Producto p : objetos) {
+				publish(new ProductoDataPanel(p, tProd, controlador));
+			}
+						
+			return null;
+		}
+	}
+	
+	public PanelMainProductos(ControladorProductos ctrl) {
+		super("Productos");
+		controlador = ctrl;
+	}
+
+	@Override
+	protected LinkedHashMap<String, JComponent> getComponentesBusqueda() {
+		LinkedHashMap<String, JComponent> map = new LinkedHashMap<>();
+
+		idTextField = createTextField();
+		map.put("ID", idTextField);
+		nombreTextField = createTextField();
+		map.put("Nombre", nombreTextField);
+		descTextField = createTextField();
+		map.put("Descripcion", descTextField);
+		stockTextField = createTextField();
+		map.put("Stock", stockTextField);
+		precioTextField = createTextField();
+		map.put("Precio", precioTextField);
+
+		JPanel lateralAdminPanel = new JPanel();
+		lateralAdminPanel.setBackground(Color.ORANGE);
+		lateralAdminPanel.setLayout(new BoxLayout(lateralAdminPanel, BoxLayout.Y_AXIS));
+		
+		return map;
+	}
+
+	@Override
+	protected AbstractPanelMain<Producto>.BuscarSwingWorker getNewSwingWorker() {
+		return new BuscarUsuarioWorker();
+	}
 
 }
---
+++ src/swsports/gui/productos/ProductoDataPanel.java
@@ -0,0 +1,87 @@
+package swsports.gui.productos;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+import swsports.gui.DataPanel;
+import swsports.modelo.Producto;
+import swsports.modelo.TransferProducto;
+
+import swsports.productos.ControladorProductos;
+
+public class ProductoDataPanel extends DataPanel<Producto> {
+
+	private ControladorProductos controlador;
+	
+	private class EditarProductoDialog extends JDialog {
+		
+		private static final long serialVersionUID = 1L;
+
+		EditarProductoDialog() {
+			super();
+			this.setTitle("Editar producto");
+			
+			//this.add(new PerfilMainPanel(object, controlador, true)); editProductoPanel
+			this.pack();
+			this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			this.setVisible(true);
+		}
+	}
+	
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			ControladorProductos ctrl = new ControladorProductos();
+			Producto prod = ctrl.consultaProducto("z9");
+
+			JFrame frame = new JFrame();
+			frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			frame.add(new ProductoDataPanel(prod, null, ctrl));
+			frame.pack();
+			frame.setVisible(true);
+		});
+	}
+
+	
+	public ProductoDataPanel(Producto prod, TransferProducto constraints, ControladorProductos ctrl) {
+		super(prod);
+		this.controlador = ctrl;
+		addData();
+		addActions();
+	}
+
+	private void addActions() {
+		addAction("Editar producto", a -> new EditarProductoDialog());
+		addAction("Dar de baja", a -> darDeBaja());
+	}
+
+	/**
+	 * Añade los campos del usuario al panel siguiendo los criterios marcados por un
+	 * {@link TransferUsuario}.
+	 */
+	private void addData() {
+		addDataField("Id", object.getId());
+		addDataField("Nombre", String.valueOf(object.getNombre()));
+		addDataField("Descripcion", object.getDesc());
+		addDataField("Stock", String.valueOf(object.getStock()));
+		addDataField("Precio", String.valueOf(object.getPrecio()));
+	}
+
+	/**
+	 * Da de baja a un {@link Producto} si el usuario de la aplicación confirma su
+	 * decisión.
+	 */
+	private void darDeBaja() {
+		String[] options = { "S�", "No" };
+		int option = JOptionPane.showOptionDialog(this, "�Seguro que quieres dar de baja este producto?",
+				"Dar producto de baja", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE, null, options,
+				options[1]);
+		if (option == JOptionPane.YES_OPTION) {
+			controlador.bajaProducto(object);
+		}
+	}
+}
