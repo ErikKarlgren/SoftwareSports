319cb3c43d06f7467284dae7fbb0dbdbddea6034
Alvaraguin <64854474+Alvaraguin@users.noreply.github.com>
2020-05-27 19:22:01 +0200

    Update FachadaProveedores.java

--- src/swsports/proveedores/FachadaProveedores.java
+++ src/swsports/proveedores/FachadaProveedores.java
@@ -1,5 +1,45 @@
 package swsports.proveedores;
 
+import swsports.modelo.TransferProveedor;
+import swsports.modelo.Proveedor;
+
 public class FachadaProveedores implements IFachadaProveedores {
 
+	private ISAProveedores sa;
+
+	public FachadaProveedores(){
+		sa = new SAProveedores();
+	}
+
+	public List<Proveedor> busquedaProveedor(TransferProveedor tProv){
+		return sa.busquedaProvedor(tProv);
+	}
+
+	public Proveedor consultaProveedor(String id){
+		return sa.consultaProveedor(id);
+	}
+
+	public boolean quitarProveedor(Proveedor prov){
+		return sa.quitarProveedor(prov);
+	}
+
+	public boolean editarProveedor(TransferProveedor tProv){
+		return sa.editarProveedor(tProv);
+	}
+
+	public boolean anyadirProveedor(Proveedor prov){
+		return sa.anyadirProveedor(prov);
+	}
+
+	public boolean pedidoProveedor(TransferProveedor tProv){
+		return sa.pedidoProveedor(tProv);
+	}
+
+	public boolean cancelarPedido(TransferProveedor tProv){
+		return sa.cancelarPedido(tProv);
+	}
+
+	public boolean recibirPedido(TransferProveedor tProv){
+		return sa.recibirPedido(tProv);
+	}
 }
