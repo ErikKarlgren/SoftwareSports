2cf9508df127e89e243ae9b7c5a95110a799ab99
javigom <64856260+javigom@users.noreply.github.com>
2020-05-28 12:17:32 +0200

    Add files via upload

--- src/swsports/modelo/Producto.java
+++ src/swsports/modelo/Producto.java
@@ -37,7 +37,7 @@ public class Producto implements Reportable {
 	 * @param stock       Número de Productos disponibles.
 	 * @param precio      Precio del producto
 	 */
-	public Producto(String id, String nombre, String desc, int stock, double precio) {
+	public Producto(String id, String nombre, String desc, Integer stock, Double precio) {
 		this.id = id;
 		this.nombre = nombre;
 		this.desc = desc;
