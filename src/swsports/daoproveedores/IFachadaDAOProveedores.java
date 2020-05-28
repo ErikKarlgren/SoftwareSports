c2ef227d913d9433ff5036dcdf1a0e89b445e6c4
matgon02 <64842504+matgon02@users.noreply.github.com>
2020-05-28 14:41:22 +0200

    Update IFachadaDAOProveedores.java

--- src/swsports/daoproveedores/IFachadaDAOProveedores.java
+++ src/swsports/daoproveedores/IFachadaDAOProveedores.java
@@ -1,5 +1,63 @@
 package swsports.daoproveedores;
 
 public interface IFachadaDAOProveedores {
+  	public List<Proveedor> busquedaProveedores(TransferProveedor tProv);
 
+	/**
+	 * Actualiza el idProducto del proveedor en cuestion.
+	 * 
+  	 * @param prov Proveedor a actualizar.
+	 * @return <code>true</code> si se ha podido actualizar correctamente.
+	 */
+	public boolean pedidoProveedor(TransferProveedor prov);
+
+	/**
+	 * Devuelve los datos de un proveedor dado su identificador (id).
+	 * 
+	 * @param id Identificador del proveedor.
+	 * @return Proveedor con el identificador buscado (puede ser nulo).
+	 */
+	public Proveedor consultaProveedor(String id);
+
+	/**
+	 * Da de baja a un proveedor.
+	 * 
+	 * @param prov Proveedor a eliminar.
+	 * @return <code>true</code> si se ha podido dar de baja al proveedor.
+	 *         <code>false</code> en caso contrario.
+	 */
+	public boolean quitarProveedor(Proveedor prov);
+	
+	/**
+	 * Edita los datos de un proveedor, con el idProducto a nulo.
+	 * 
+	 * @param rProv          Proveedor actualizado.
+	 * @return <code>true</code> si se ha podido editar al proveedor.
+	 */
+	public boolean recibirPedido(TransferProveedor tProv);
+
+	/**
+	 * Edita los datos de un proveedor.
+	 * 
+	 * @param rProv          Proveedor actualizado.
+	 * @return <code>true</code> si se ha podido editar al proveedor.
+	 */
+	public boolean editarProveedor(TransferProveedor tProv);
+
+	/**
+	 * Cancela el pedido con el proveedor.
+	 * 
+	 * @param rProv          Proveedor actualizado con el idProducto a nulo.
+	 * @return <code>true</code> si se ha podido editar los datos.
+	 */
+	public boolean cancelarPedido(TransferProveedor tProv);
+
+	/**
+	 * Da de alta un proveedor y lo registra en la apliación.
+	 * 
+	 * @param prov Proveedor a registrar
+	 * @return <code>true</code> si se ha podido registrar al proveedor.
+	 *         <code>false</code> en caso contrario.
+	 */
+	public boolean anyadirProveedor(Proveedor prov);
 }
