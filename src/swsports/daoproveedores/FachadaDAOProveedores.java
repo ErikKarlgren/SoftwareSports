b05e283cfc619d15b3aa79672c31ed51c6818edd
matgon02 <64842504+matgon02@users.noreply.github.com>
2020-05-28 14:40:40 +0200

    Update FachadaDAOProveedores.java

--- src/swsports/daoproveedores/FachadaDAOProveedores.java
+++ src/swsports/daoproveedores/FachadaDAOProveedores.java
@@ -1,5 +1,58 @@
 package swsports.daoproveedores;
 
 public class FachadaDAOProveedores implements IFachadaDAOProveedores {
+  
+	private IDAOProveedores dao;
 
+	public FachadaDAOProveedores() {
+		dao = new DAOProveedores();
+	}
+
+	@Override
+	public List<Proveedor> busquedaProveedores(TransferProveedor tProv) {
+		// TODO Auto-generated method stub
+		return dao.busquedaProveedores(tProv);
+	}
+
+	@Override
+	public boolean pedidoProveedor(TransferProveedor prov) {
+		// TODO Auto-generated method stub
+		return dao.pedidoProveedor(prov);
+	}
+
+	@Override
+	public Proveedor consultaProveedor(String id) {
+		// TODO Auto-generated method stub
+		return dao.consultaProveedor(id);
+	}
+
+	@Override
+	public boolean quitarProveedor(Proveedor prov) {
+		// TODO Auto-generated method stub
+		return dao.quitarProveedor(prov);
+	}
+
+	@Override
+	public boolean recibirPedido(TransferProveedor tProv) {
+		// TODO Auto-generated method stub
+		return dao.recibirPedido(tProv);
+	}
+
+	@Override
+	public boolean editarProveedor(TransferProveedor tProv) {
+		// TODO Auto-generated method stub
+		return dao.editarProveedor(tProv);
+	}
+
+	@Override
+	public boolean cancelarPedido(TransferProveedor tProv) {
+		// TODO Auto-generated method stub
+		return dao.cancelarPedido(tProv);
+	}
+
+	@Override
+	public boolean anyadirProveedor(Proveedor prov) {
+		// TODO Auto-generated method stub
+		return dao.anyadirProveedor(prov);
+	}
 }
