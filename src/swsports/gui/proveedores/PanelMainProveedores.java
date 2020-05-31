78e2365af2183739ab03c7ee31ea82f211f7e7c1
matgon02 <64842504+matgon02@users.noreply.github.com>
2020-05-31 13:10:40 +0200

    Add files via upload

--- src/swsports/gui/proveedores/PanelMainProveedores.java
+++ src/swsports/gui/proveedores/PanelMainProveedores.java
@@ -1,7 +1,153 @@
 package swsports.gui.proveedores;
 
-import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedHashMap;
 
-public class PanelMainProveedores extends JPanel  {
+import swsports.modelo.Producto;
+import swsports.modelo.Proveedor;
+import swsports.modelo.TransferProveedor;
+import swsports.modelo.TransferUsuario;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+
+import swsports.gui.AbstractPanelMain;
+import swsports.gui.MainWindow;
+import swsports.proveedores.ControladorProveedores;
+
+public class PanelMainProveedores extends AbstractPanelMain<Proveedor> {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private JTextField idTextField;
+	private JTextField nombreTextField;
+	private JTextField descTextField;
+	private JTextField idProdTextField;
+	private JTextField stockTextField;
+	private ControladorProveedores controlador;	
+	private Producto prod;
+	
+	private class BuscarProveedorWorker extends BuscarSwingWorker {
+
+		@Override
+		protected Void doInBackground() throws Exception {
+			setSearchButtonEnabled(false);
+
+			String id = idTextField.getText().equals("") ? null : idTextField.getText();
+			String nombre = nombreTextField.getText().equals("") ? null : nombreTextField.getText();
+			String desc =  descTextField.getText().equals("") ? null : descTextField.getText();
+			String idProd = idProdTextField.getText().equals("") ? null : idProdTextField.getText();
+			prod = controlador.busquedaProducto(idProd);
+			
+			TransferProveedor tProv = new TransferProveedor(id, nombre, desc, prod, stock);
+			objetos = controlador.busquedaProveedores(tProv);
+			
+			removeReportablePanels();	
+			for (Proveedor p : objetos) {
+				publish(new ProveedorDataPanel(p, controlador));
+			}
+						
+			return null;
+		}
+	}
+	
+
+
+	public PanelMainProveedores(ControladorProveedores ctrlProv) {
+		super("Proveedores");
+		controlador = ctrlProv;
+		this.initGUIProveedores();
+		// TODO Auto-generated constructor stub
+	}
+
+	private void initGUIProveedores() {
+		JPanel botones = new JPanel();
+		JButton anyadirProveedor = new JButton("Anyadir Proveedor");
+		botones.add(anyadirProveedor);
+		anyadirProveedor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				new AnyadirProveedorDialog();
+			}
+		});
+		this.add(botones, BorderLayout.SOUTH);
+	}
+	
+	private class AnyadirProveedorDialog extends JDialog {
+		private static final long serialVersionUID = 1L;
+		
+		private JTextField id;
+		private JTextField nombre;
+		private JTextField descripcion;
+		
+		AnyadirProveedorDialog() {
+			super();
+			this.setTitle("Anyadir proveedor");
+			this.setLayout(new FlowLayout());
+			
+			JLabel idP = new JLabel("ID");
+			this.add(idP);
+			id = new JTextField();
+			id.setPreferredSize(new Dimension(100, 20));
+			this.add(id);
+			JLabel nombreP = new JLabel("Nombre");
+			this.add(nombreP);
+			nombre = new JTextField();
+			nombre.setPreferredSize(new Dimension(100, 20));
+			this.add(nombre);
+			JLabel descP = new JLabel("Descripcion");
+			this.add(descP);
+			descripcion = new JTextField();
+			descripcion.setPreferredSize(new Dimension(100, 20));
+			this.add(descripcion);
+			JButton anyadir = new JButton("Anyadir");
+			anyadir.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					new AnyadirProveedorDialog();
+				}
+			});
+			
+			this.pack();
+			this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			this.setVisible(true);
+		}
+	}
+	
+	@Override
+	protected LinkedHashMap<String, JComponent> getComponentesBusqueda() {
+		// TODO Auto-generated method stub
+		LinkedHashMap<String, JComponent> map = new LinkedHashMap<>();
+
+		idTextField = createTextField();
+		map.put("ID", idTextField);
+		nombreTextField = createTextField();
+		map.put("Nombre", nombreTextField);
+		descTextField = createTextField();
+		map.put("Telefono", descTextField);
+		idProdTextField = createTextField();
+		map.put("ID Producto", idProdTextField);
+		stockTextField = createTextField();
+		map.put("Cantidad", stockTextField);
+		stockTextField = createTextField();
+		map.put("Cantidad", stockTextField);
+
+		return map;
+	}
+
+	@Override
+	protected AbstractPanelMain<Proveedor>.BuscarSwingWorker getNewSwingWorker() {
+		return new BuscarProveedorWorker();
+	}
 
 }
---
+++ src/swsports/gui/proveedores/ProveedorDataPanel.java
@@ -0,0 +1,144 @@
+package swsports.gui.proveedores;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+import swsports.gui.DataPanel;
+import swsports.gui.PerfilMainPanel;
+import swsports.gui.ProveedorMainPanel;
+import swsports.gui.usuarios.UsuarioDataPanel;
+import swsports.modelo.Proveedor;
+import swsports.modelo.TransferProveedor;
+import swsports.modelo.TransferUsuario;
+import swsports.modelo.Usuario;
+import swsports.proveedores.ControladorProveedores;
+import swsports.usuarios.ControladorUsuario;
+
+public class ProveedorDataPanel extends DataPanel<Proveedor> {
+
+	private boolean hayPedido = false;
+	/**
+	 * Diálogo que muestra el panel para editar el perfil de un usuario, incluyendo
+	 * si concederle o revocarle privilegios de administrador.
+	 */
+	private class EditarProveedorDialog extends JDialog {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		
+		EditarProveedorDialog() {
+			super();
+			this.setTitle("Editar proveedor");
+			this.add(new ProveedorMainPanel(object, controlador));
+			this.pack();
+			this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			this.setVisible(true);
+		}
+	}
+	
+	private class CrearPedidoDialog extends JDialog{
+		private static final long serialVersionUID = 1L;
+		
+		private boolean hayPedido = false;
+
+		CrearPedidoDialog() {
+			super();
+			this.setTitle("Crear pedido");
+			this.add(new ProveedorMainPanel(object, controlador));
+			this.pack();
+			this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			this.setVisible(true);
+		}
+	}
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			ControladorProveedores ctrl = new ControladorProveedores();
+			Proveedor prov = ctrl.consultaProveedor("z9");
+
+			JFrame frame = new JFrame();
+			frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			frame.add(new ProveedorDataPanel(prov, null, ctrl));
+			frame.pack();
+			frame.setVisible(true);
+		});
+	}
+
+	private ControladorProveedores controlador;
+
+	/**
+	 * Crea un panel con los datos de un {@link Usuario} y siguiendo las
+	 * restricciones guardadas en un {@link TransferUsuario} (los atributos nulos
+	 * implican que sus datos correspondientes no se mostrarán). El
+	 * {@link ControladorUsuario} es necesario para poder realizar las acciones de
+	 * editar el usuario o darlo de baja.
+	 * 
+	 * @param usu         {@link Usuario} del que queremos mostrar sus datos.
+	 * @param constraints Restricciones a la hora de mostrar o no los atributos de
+	 *                    <code>usu</code>. Los atributos nulos de
+	 *                    <code>constraints</code> implican que los atributos
+	 *                    correspondientes de <code>usu</code> no se mostrarán. Si
+	 *                    <code>constraints</code> es nulo se mostrarán todos los
+	 *                    datos.
+	 * @param ctrl        Controlador para poder interactuar con el resto del
+	 *                    programa.
+	 */
+	public ProveedorDataPanel(Proveedor prov, ControladorProveedores ctrl) {
+		super(prov);
+		this.controlador = ctrl;
+		addData();
+		addActions();
+	}
+
+	/**
+	 * Añade las acciones del panel: editar un usuario y darlo de baja.
+	 */
+	private void addActions() {
+		addAction("Editar proveedor", a -> new EditarProveedorDialog());
+		addAction("Hacer Pedido", a -> new CrearPedidoDialog());
+		addAction("Recibir Pedido", a -> eliminarPedido());
+		addAction("Quitar Pedido", a -> eliminarPedido());
+		addAction("Eliminar Proveedor", a -> darDeBaja());
+	}
+
+	private Object eliminarPedido() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Añade los campos del usuario al panel siguiendo los criterios marcados por un
+	 * {@link TransferUsuario}.
+	 */
+	private void addData() {
+		addDataField("Nombre", object.getNombre());
+		addDataField("Descripcion", object.getDesc());
+		if(hayPedido == true) {
+			addDataField("Producto", object.getIdProducto());
+			addDataField("Cantidad", Integer.toString(object.getStock()));
+			addDataField("Precio Total", Double.toString(object.getPrecio()));
+		}
+	}
+
+	/**
+	 * Da de baja a un {@link Usuario} si el usuario de la aplicación confirma su
+	 * decisión.
+	 */
+	private void darDeBaja() {
+		String[] options = { "Sí", "No" };
+		int option = JOptionPane.showOptionDialog(this, "¿Seguro que quieres eliminar a este proveedor?",
+				"Eliminar proveedor", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE, null, options,
+				options[1]);
+		if (option == JOptionPane.YES_OPTION) {
+			controlador.eliminarProveedor(object.getId());
+		}
+	}
+}
